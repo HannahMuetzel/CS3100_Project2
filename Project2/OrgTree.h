25431cf5255eb82d056922b1b42343bcbfe97c08
Hannah Muetzel <muetzel.2@wright.edu>
2016-03-14 17:22:21 -0400

    Prototyping
    Functions in OrgTree.cpp prototyped here in OrgTree.h.
    TODO: prototype resize function
    TODO: add privates

--- Project2/OrgTree.h
+++ Project2/OrgTree.h
@@ -1,8 +1,25 @@
 #pragma once
+#define TREENODEPTR unsigned int
+#define TREENULLPTR	-1
+#include <string>
+
 class OrgTree
 {
 public:
 	OrgTree();
 	~OrgTree();
-};
 
+	//TODO: prototype resize tree if full and adding more crap to it
+	void setPointers(parent, lc, rs);
+	void addRoot(title, name);
+	unsigned int getSize();
+	unsigned int getRoot();
+	unsigned int leftmostChild(TREENODEPTR node);
+	unsigned int rightSibling(TREENODEPTR node);
+	void printSubTree(TREENODEPTR subTreeRoot);
+	unsigned int find(string title);
+	bool read(string filename);
+	void write(string filename);
+	void hire(TREENODEPTR parent, string title, string name);
+	bool fire(string title);
+};
