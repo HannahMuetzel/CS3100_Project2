1395991b901075ff92c52d43788a15a9e1625a2d
Hannah Muetzel <muetzel.2@wright.edu>
2016-03-17 23:18:17 -0400

    JENGA!!!!!!! 7
    i'm dead inside

--- Project2/TestC0de.cpp
+++ Project2/TestC0de.cpp
@@ -8,11 +8,8 @@ int main()
 	TreeNode nubsFriend("Nubbin 2", "Nubbin 1's Friend", TREENULLPTR, TREENULLPTR, TREENULLPTR);
 	
 	OrgTree cherryTree;
-	//TODO: test OrgTree::addRoot() & OrgTree::setPointers()
 	cherryTree.addRoot("test title", "test name");
-	//TODO: test OrgTree::resize()
-	//OrgTree::addRoot(string "new title", string "new name");
-
+	
 	std::cout << "Boo!" << std::endl;
 	system("pause");
 	return 0;
